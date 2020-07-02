b5794a4cd67c336d030f2590db87c248e35db389
chessann123 <55215674+chessann123@users.noreply.github.com>
2020-07-02 14:23:00 -0700

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -3,7 +3,7 @@ class Dustbin{
         side1 = new Ground(900,370,200,20, "blue");
         side2 = new Ground(800,320,20,100, "blue");
         side3 = new Ground(1000,320,20,100, "blue");
-        this.image = loadImage("trash.png");
+        //this.image = loadImage("trash.png");
     }
     display(){
         side1.display();
